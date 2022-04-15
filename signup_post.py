cceeb60ef5fcf0fc507917455f4a0edd2a6a19b7
AJ <jinaru.andy@gmail.com>
2022-04-15 11:37:01 +0200

    User Name - Sign Up

--- signup_post.py
+++ signup_post.py
@@ -8,7 +8,10 @@ def _():
     user_id = str(uuid.uuid4())
     user_email = request.forms.get("user_email")
     user_password = request.forms.get("user_password")
-    user = {"user_id":user_id, "user_email":user_email, "user_password":user_password} 
+    user_last_name = request.forms.get("last_name")
+    user_first_name = request.forms.get("first_name")
+    user = {"user_id":user_id, "user_email":user_email, "user_password":user_password, "last_name":user_last_name, "first_name":user_first_name} 
+    print(user)
     g.USERS.append(user)
     return redirect("/login")
 
